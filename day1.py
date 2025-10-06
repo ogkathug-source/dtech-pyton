4800a24caff405da41f50beb7112d829b3d53500
ogkathug-source <ogkathug@gmail.com>
2025-10-06 14:43:06 +0300

    first python commit

---
+++ day1.py
@@ -0,0 +1,7 @@
+# # My name is Nelson
+
+# My_name="Nelson"
+# print(My_name)
+
+name=input("what is your name?:")
+print(name)
---
+++ day4.py
@@ -0,0 +1,22 @@
+# this programe checks on the user inputs and validate it
+
+print ("This programe needs credentials to varify them to grant access to user")
+print
+
+username= input ("Enter your username:")
+password= input ("Enter your password:")
+
+
+if username=="kathug" and password=="kathug1234"
+    print("signed in successfully!!")
+
+elif username!="kathug" and password=="kathug1234":
+      print("signed in successfully!!")
+
+else:
+    print("wrong credentials")
+
+
+
+
+
